19d67f236bede5b15de398d7dc67e61330d5b98e
bintang ary <wardhanabintang07@gmail.com>
2024-08-30 22:28:50 +0700

    Fixing occupants table

---
+++ app/Http/Controllers/OccupantsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Occupants;
+use App\Http\Requests\StoreOccupantsRequest;
+use App\Http\Requests\UpdateOccupantsRequest;
+
+class OccupantsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOccupantsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Occupants $occupants)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Occupants $occupants)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateOccupantsRequest $request, Occupants $occupants)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Occupants $occupants)
+    {
+        //
+    }
+}
--- database/migrations/2024_08_21_170059_create_occupants_table.php
+++ database/migrations/2024_08_21_170059_create_occupants_table.php
@@ -11,27 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('occupants', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('born');
-            $table->string('dom');
-            $table->string('phone_number');
-            $table->string('email')->unique();
+            $table->string('occ_name');
+            $table->string('occ_username');
+            $table->string('occ_dom');
+            $table->string('occ_email');
+            $table->string('occ_phone');
+            $table->string('occ_password');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+        Schema::create('occupants_password_reset_tokens', function (Blueprint $table) {
+            $table->string('occ_email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('occupants_sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
@@ -41,12 +40,13 @@ return new class extends Migration
         });
     }
 
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('occupants');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
